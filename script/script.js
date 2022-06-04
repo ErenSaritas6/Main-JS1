7bd8c6d4504ccbe7639da91cd82a673dd9c400e9
Eren Sarıtaş <79216477+ErenSaritas6@users.noreply.github.com>
2022-06-04 13:49:34 +0300

    Update script.js
    counter update

--- script/script.js
+++ script/script.js
@@ -11,31 +11,40 @@ function computerPlay() {
     }
 }
 
+let countPlayer = 0;
+let countComputer = 0;
+
 function playOneRound (playerSelection, computerSelection) {
 
     const playerSelectionUp = playerSelection.toLowerCase();
 
     if (playerSelectionUp === `rock` && computerSelection === `scissors`) {
+        countPlayer++;
         return `You Win! Rock beats Scissors.`;
     }
 
     else if (playerSelectionUp === `paper` && computerSelection === `rock`) {
+        countPlayer++;
         return `You Win! Paper beats Rock.`;
     }
 
     else if (playerSelectionUp === `scissors` && computerSelection === `paper`) {
+        countPlayer++;
         return `You Win! Scissors beats Paper.`;
     }
 
     else if (playerSelectionUp === `scissors` && computerSelection === `rock`) {
+        countComputer++;
         return `You Lose! Rock beats Scissors.`;
     }
 
     else if (playerSelectionUp === `rock` && computerSelection === `paper`) {
+        countComputer++;
         return `You Lose! Paper beats Rock.`;
     }
 
     else if (playerSelectionUp === `paper` && computerSelection === `scissors`) {
+        countComputer++;
         return `You Lose! Scissors beats Paper.`;
     }
 
@@ -55,6 +64,16 @@ function game() {
         console.log(`You: ${playerSelection}`);
         console.log(`Computer: ${computerSelection}`);
         console.log(`Result: ${playOneRound(playerSelection, computerSelection)}`);
+    }
+
+    if (countPlayer > countComputer) {
+        console.log(`Final Result: It is ${countPlayer} to ${countComputer}. You Win!`);
+    }
+    else if (countComputer > countPlayer) {
+        console.log(`Final Result: It is ${countComputer} to ${countPlayer}. You Lose!`);
+    }
+    else {
+        console.log(`Final Result: It is a draw!`);
     }
 }
 
